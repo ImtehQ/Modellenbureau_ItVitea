28b5e4684f51f1507e229108eef5113dbd44e89f
ImtehQ <its_q@hotmail.com>
2021-06-14 11:33:35 +0200

    Add project files.

---
+++ Models/ModelUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Modellenbureau.Models
+{
+    public class ModelUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public byte Age { get; set; }
+        public int BodySize { get; set; }
+        public List<Photo> Photos { get; set; }
+    }
+
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string ImgUrl { get; set; }
+        public string ImgData { get; set; }
+    }
+}
